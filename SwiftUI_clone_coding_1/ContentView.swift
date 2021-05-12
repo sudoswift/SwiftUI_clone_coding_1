4005d77e092e78d0ad1fc72f75d3fa6788b11d8f
sudoswift <sudoswift@gmail.com>
2021-05-13 00:04:37 +0900

    Initial Commit

---
+++ SwiftUI_clone_coding_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_clone_coding_1
+//
+//  Created by izvern on 2021/05/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_clone_coding_1/SwiftUI_clone_coding_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_clone_coding_1App.swift
+//  SwiftUI_clone_coding_1
+//
+//  Created by izvern on 2021/05/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_clone_coding_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
