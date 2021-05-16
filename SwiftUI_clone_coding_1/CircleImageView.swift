a0533bf49826610519e48b099b4f4bad2726aaf3
sudoswift <sudoswift@gmail.com>
2021-05-17 00:44:39 +0900

    #6 ing2

--- SwiftUI_clone_coding_1/CircleImageView.swift
+++ SwiftUI_clone_coding_1/CircleImageView.swift
@@ -16,10 +16,13 @@ struct CircleImageView: View {
 //                .shadow(color: .gray, radius: 2, x: 10, y: 10)
             Image("myImage")
                 .resizable() // Image 에서는 resizable modifier이 국룰
-                .aspectRatio(contentMode: .fill) // 사진 비율 선택
-                .frame(width:200) // frame 밑에 clipped를 써서 사진을 자를 수 있다.
+                .scaledToFit()
+                .frame(width: 200, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+//                .aspectRatio(contentMode: .fill) // 사진 비율 선택
+//                .frame(width:200) // frame 밑에 clipped를 써서 사진을 자를 수 있다.
                 .clipped()
                 .edgesIgnoringSafeArea(.all) // 화면에 꽉차게 채울지 말지 선택
+                .clipShape(Circle()) // make image circle
                 
         }
     }
