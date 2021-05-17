f067b60790b9866e3694f4fa25bd536882b02d3f
sudoswift <sudoswift@gmail.com>
2021-05-18 00:53:22 +0900

    create MyZstack

---
+++ SwiftUI_clone_coding_1/MyZstack.swift
@@ -0,0 +1,22 @@
+//
+//  MyZstack.swift
+//  SwiftUI_clone_coding_1
+//
+//  Created by izvern on 2021/05/18.
+//
+
+import SwiftUI
+
+struct MyZstack: View {
+    var body: some View {
+        ZStack{
+            Rectangle()
+        }
+    }
+}
+
+struct MyZstack_Previews: PreviewProvider {
+    static var previews: some View {
+        MyZstack()
+    }
+}
