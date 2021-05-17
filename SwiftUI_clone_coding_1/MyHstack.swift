a15c034126949f5ba1c647fe3b7bbaafc363c951
sudoswift <sudoswift@gmail.com>
2021-05-18 00:46:33 +0900

    create myhstack

---
+++ SwiftUI_clone_coding_1/MyHstack.swift
@@ -0,0 +1,20 @@
+//
+//  MyHstack.swift
+//  SwiftUI_clone_coding_1
+//
+//  Created by izvern on 2021/05/18.
+//
+
+import SwiftUI
+
+struct MyHstack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyHstack_Previews: PreviewProvider {
+    static var previews: some View {
+        MyHstack()
+    }
+}
