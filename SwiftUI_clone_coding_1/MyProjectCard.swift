4457d0ffc87ff49dc148ef93de39ba7d3f836d34
sudoswift <sudoswift@gmail.com>
2021-05-19 02:26:04 +0900

    make stroke to Image View in MyProjectCard

--- SwiftUI_clone_coding_1/MyProjectCard.swift
+++ SwiftUI_clone_coding_1/MyProjectCard.swift
@@ -24,15 +24,26 @@ struct MyProjectCard: View {
                     .resizable()  //image view 바로 밑에 resizable modifier는 국룰
                     .clipShape(Circle())
                     .frame(width:50, height: 50)
+                    .overlay( // image view에 경계선을 넣으려면 overlay modifier를 활용해 다음과 같이 stroke 수정자를 활용한다. 경계선의 색은 Color로 선의 굵기는 lineWidth로 설정해준다.
+                        Circle()
+                            .stroke(Color.blue, lineWidth: 3.5)
+                    )
                 Image("2")
                     .resizable()
                     .clipShape(Circle())
                     .frame(width:50, height: 50)
+                    .overlay(
+                        Circle()
+                            .stroke(Color.green, lineWidth: 3.5)
+                    )
                 Image("3")
                     .resizable()
                     .clipShape(Circle())
                     .frame(width:50, height: 50)
-                
+                    .overlay(
+                        Circle()
+                            .stroke(Color.red, lineWidth: 3.5)
+                    )
                 Spacer()
                 
                 Text("확인")
