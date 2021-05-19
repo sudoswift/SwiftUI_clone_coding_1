051e90254582b8eda7b6b0914eb69bb6248b634a
sudoswift <sudoswift@gmail.com>
2021-05-20 01:18:11 +0900

    list 부분 끝

--- SwiftUI_clone_coding_1/MyList.swift
+++ SwiftUI_clone_coding_1/MyList.swift
@@ -13,18 +13,30 @@ struct MyList: View {
         
         // List view 속 section view!!
         List{
-            Section(header: Text("오늘 할 일")){
+            Section(
+                header: Text("오늘 할 일")
+                    .font(.headline)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.black)
+                ,footer: Text("footer"))
+            {
                 ForEach(1...3, id: \.self) { itemIndex in
                     MyCard(icon: "book.fill", title: "책읽기 \(itemIndex)", start: "1PM", end: "3PM", bgColor: Color.green)
             }
         }
             .listRowInsets(EdgeInsets.init(top: 10, leading: 10, bottom: 10, trailing: 10))
-            Section(header: Text("내일 할 일")){
-                ForEach(1...3, id: \.self) { itemIndex in
+            
+            Section(
+                header: Text("내일 할 일")
+                    .font(.headline)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.black))
+            {
+                ForEach(1...20, id: \.self) { itemIndex in
                     MyCard(icon: "book.fill", title: "책읽기 \(itemIndex)", start: "1PM", end: "3PM", bgColor: Color.blue)
                 }
             }
-            .listRowInsets(EdgeInsets.init(top: 10, leading: 30, bottom: 10, trailing: 10))
+            .listRowInsets(EdgeInsets.init(top: 10, leading: 10, bottom: 10, trailing: 10))
             
             .listRowBackground(Color.yellow) // list의 section 속 backgroundColor 지정
         }
