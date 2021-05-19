6150360370eb42ceb7293179d7eccdd36f121021
sudoswift <sudoswift@gmail.com>
2021-05-20 00:49:32 +0900

    #10 about list

---
+++ SwiftUI_clone_coding_1/MyList.swift
@@ -0,0 +1,20 @@
+//
+//  MyList.swift
+//  SwiftUI_clone_coding_1
+//
+//  Created by izvern on 2021/05/20.
+//
+
+import SwiftUI
+
+struct MyList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyList_Previews: PreviewProvider {
+    static var previews: some View {
+        MyList()
+    }
+}
