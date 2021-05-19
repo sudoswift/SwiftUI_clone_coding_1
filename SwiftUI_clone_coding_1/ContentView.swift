183a518ec2cb705fd00049c4815c73f8df55d0be
sudoswift <sudoswift@gmail.com>
2021-05-19 18:24:02 +0900

    #8 complete !!

--- SwiftUI_clone_coding_1/ContentView.swift
+++ SwiftUI_clone_coding_1/ContentView.swift
@@ -10,12 +10,25 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-            ZStack(alignment: .bottomTrailing){
-                VStack{
-                    
+        ZStack(alignment: .bottomTrailing){
+            
+            VStack(alignment: .leading){
+                HStack{
+                    Image(systemName: "line.horizontal.3")
+                        .font(.largeTitle)
+                    Spacer()
+                    Image(systemName: "person.crop.circle.fill")
+                        .font(.largeTitle)
                 }
+                .padding()
+                Text("SUDO 클론 프로젝트")
+                    .font(.system(size: 30))
+                    .foregroundColor(.black)
+                    .fontWeight(.bold)
+                    .padding()
+                
                 ScrollView{ //add scrollView
-                    LazyVStack{
+                    VStack{
                         MyProjectCard()
                         MyBasicCard()
                         MyBasicCard()
@@ -23,13 +36,16 @@ struct ContentView: View {
                         MyBasicCard()
                         MyBasicCard()
                         MyBasicCard()
+                        MyBasicCard()
+                        MyBasicCard()
                     }
-                    .padding(8)
                 }
-
+            }
+            .padding(.top, 10)
+            .padding(.horizontal, 20)
                 Circle()
                     .foregroundColor(.yellow)
-                    .frame(width: 80, height: 80)
+                    .frame(width: 60, height: 60)
                     .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 10)
                     .overlay(
                         Image(systemName: "plus")
@@ -45,6 +61,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+            ContentView()
     }
 }
