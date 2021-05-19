3ff30367b86493baeffd3710f5db99aa5af04c7d
sudoswift <sudoswift@gmail.com>
2021-05-19 17:50:52 +0900

    add circle button using zstack and overlay modifier

--- SwiftUI_clone_coding_1/ContentView.swift
+++ SwiftUI_clone_coding_1/ContentView.swift
@@ -10,10 +10,29 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
+        ZStack(alignment: .bottomTrailing){
             LazyVStack{
                 MyProjectCard()
-                MyProjectCard()
+                MyBasicCard()
+                MyBasicCard()
+                MyBasicCard()
+                MyBasicCard()
+                MyBasicCard()
+                MyBasicCard()
             }
+            .padding(8)
+            Circle()
+                .foregroundColor(.yellow)
+                .frame(width: 80, height: 80)
+                .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 5, x: 2, y: 3)
+                .overlay(
+                    Image(systemName: "plus")
+                        .font(.title)
+                        .foregroundColor(.white)
+                )
+                .padding(.trailing, 10)
+        }
+
     }
 }
 
