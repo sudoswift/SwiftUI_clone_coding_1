bd6db16666d577ffa64d70e486df6c85b9be531f
sudoswift <sudoswift@gmail.com>
2021-05-19 17:57:38 +0900

    add ScrollView!!

--- SwiftUI_clone_coding_1/ContentView.swift
+++ SwiftUI_clone_coding_1/ContentView.swift
@@ -10,28 +10,35 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        ZStack(alignment: .bottomTrailing){
-            LazyVStack{
-                MyProjectCard()
-                MyBasicCard()
-                MyBasicCard()
-                MyBasicCard()
-                MyBasicCard()
-                MyBasicCard()
-                MyBasicCard()
+            ZStack(alignment: .bottomTrailing){
+                VStack{
+                    
+                }
+                ScrollView{ //add scrollView
+                    LazyVStack{
+                        MyProjectCard()
+                        MyBasicCard()
+                        MyBasicCard()
+                        MyBasicCard()
+                        MyBasicCard()
+                        MyBasicCard()
+                        MyBasicCard()
+                    }
+                    .padding(8)
+                }
+
+                Circle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 80, height: 80)
+                    .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 10)
+                    .overlay(
+                        Image(systemName: "plus")
+                            .font(.system(size: 30))
+                            .foregroundColor(.white)
+                    )
+                    .padding(10)
             }
-            .padding(8)
-            Circle()
-                .foregroundColor(.yellow)
-                .frame(width: 80, height: 80)
-                .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 5, x: 2, y: 3)
-                .overlay(
-                    Image(systemName: "plus")
-                        .font(.title)
-                        .foregroundColor(.white)
-                )
-                .padding(.trailing, 10)
-        }
+
 
     }
 }
