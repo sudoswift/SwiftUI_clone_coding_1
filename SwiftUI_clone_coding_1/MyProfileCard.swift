35e0d2a3260a6388d653558ed77a904e0e5ef951
sudoswift <sudoswift@gmail.com>
2021-05-20 17:09:44 +0900

    create MyProfileCard

---
+++ SwiftUI_clone_coding_1/MyProfileCard.swift
@@ -0,0 +1,20 @@
+//
+//  MyProfileCard.swift
+//  SwiftUI_clone_coding_1
+//
+//  Created by izvern on 2021/05/20.
+//
+
+import SwiftUI
+
+struct MyProfileCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyProfileCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MyProfileCard()
+    }
+}
