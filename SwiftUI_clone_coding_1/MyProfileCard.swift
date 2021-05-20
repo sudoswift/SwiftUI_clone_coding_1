8a4d605fdfad60c2f0840c880fd1f4a3764d34f0
sudoswift <sudoswift@gmail.com>
2021-05-20 17:18:51 +0900

    little chagne

--- SwiftUI_clone_coding_1/MyProfileCard.swift
+++ SwiftUI_clone_coding_1/MyProfileCard.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct MyProfileCard: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image("아이번")
+                .resizable()
+                .clipShape(Circle())
+                .scaledToFit()
+                .overlay(
+                    Circle()
+                        .stroke(Color.yellow, lineWidth: 20)
+                )
+                .frame(width: 350)
+                .shadow(color: .black, radius: 10, x: 5, y: 5)
+                .padding(.bottom, 40)
+            Text("SUDO SwiftUI")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.bottom)
+            Text("깃허브 Fork 부탁드립니다!!")
+                .font(.title)
+                .fontWeight(.bold)
+            HStack{
+                Text("깃허브 가기")
+                
+            }
+        }
     }
 }
 
