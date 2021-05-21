9fcf5e29c85c3dc27aac0a98a811d305242b55a6
sudoswift <sudoswift@gmail.com>
2021-05-22 00:42:27 +0900

    #13 about TabView

---
+++ SwiftUI_clone_coding_1/MyTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MyTabView.swift
+//  SwiftUI_clone_coding_1
+//
+//  Created by izvern on 2021/05/22.
+//
+
+import SwiftUI
+
+struct MyTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyTabView()
+    }
+}
